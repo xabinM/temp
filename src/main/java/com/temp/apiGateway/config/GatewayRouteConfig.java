0932824218929cedda41559a8647fb890e72039e
xabinM <binine2@gmail.com>
2025-12-16 17:28:09 +0900

    initial commit

---
+++ src/main/java/com/temp/apiGateway/config/GatewayRouteConfig.java
@@ -0,0 +1,19 @@
+package com.temp.apiGateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRouteConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", r -> r
+                        .path("/api/auth/**")
+                        .uri("lb://auth-service"))
+                .build();
+    }
+}
