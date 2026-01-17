b3950c740737f2ba7a006403d7048db55200e81e
xabinM <binine2@gmail.com>
2026-01-17 19:24:12 +0900

    refactor: commonModule로 response code & exception 이전

--- commonModule/src/main/java/temp/commonModule/code/ErrorCode.java
+++ commonModule/src/main/java/temp/commonModule/code/ErrorCode.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "Member not found"),
+    MEMBER_ALREADY_EXIST(HttpStatus.CONFLICT, "Member already exists"),
+    JWT_KEY_LOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to load JWT key"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
 
     private final HttpStatus status;
